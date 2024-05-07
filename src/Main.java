7b029f3de85c85118f243f0e571d4497068d5fa5
Anastasiia Tokareva <wwwnasty11@rambler.ru>
2024-05-07 19:39:35 +0300

    .gitignore

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int clientAccount = 100;
+        int bonusClient = 100;
+        int allAmount = 1200;
+
+        int bonus;
+
+        if(allAmount>=1000) {
+            bonus = allAmount / bonusClient;
+        } else {
+            bonus = 0;
+        }
+
+        int Account = clientAccount + allAmount + bonus;
+
+        System.out.println ("При пополнении баланса на сумму" + allAmount + "руб. бонус составил сумму" + bonus + "руб. Ваш баланс" + Account + "руб.");
+    }
+}
